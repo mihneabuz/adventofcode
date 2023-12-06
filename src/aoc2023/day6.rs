a34e0f1ad84d112717b7c55d48d9cb85e80f695a
mihneabuz <mihneabuzatu88@gmail.com>
2023-12-06 11:34:31 +0200

    improve 2023 day6

--- src/aoc2023/day6.rs
+++ src/aoc2023/day6.rs
@@ -1,4 +1,6 @@
 use itertools::Itertools;
+use num::integer::Roots;
+
 use lib::{aoc, challenge::Challenge};
 
 pub struct Day6;
@@ -7,45 +9,63 @@ impl Challenge for Day6 {
     aoc!(year = 2023, day = 6);
 
     fn solve(input: String) -> (String, String) {
-        let (time_line, dist_line) = input.lines().collect_tuple().unwrap();
-
-        let times = time_line
-            .split_whitespace()
-            .skip(1)
-            .map(|n| n.parse::<usize>().unwrap())
-            .collect_vec();
-
-        let dists = dist_line
-            .split_whitespace()
-            .skip(1)
-            .map(|n| n.parse::<usize>().unwrap())
-            .collect_vec();
-
-        let fst = times
-            .iter()
-            .copied()
-            .zip(dists.iter().copied())
-            .map(|(time, dist)| (1..time).filter(|x| x * (time - x) > dist).count())
-            .product::<usize>();
-
-        let time = times
-            .into_iter()
-            .map(|t| t.to_string())
-            .reduce(|acc, s| acc + &s)
-            .unwrap()
-            .parse::<usize>()
+        let (times, dists) = input
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .skip(1)
+                    .map(|s| s.parse::<isize>().unwrap())
+                    .collect_vec()
+            })
+            .collect_tuple()
             .unwrap();
 
-        let dist = dists
-            .into_iter()
-            .map(|t| t.to_string())
-            .reduce(|acc, s| acc + &s)
-            .unwrap()
-            .parse::<usize>()
+        let fst = (0..times.len())
+            .map(|i| solve(times[i], dists[i]))
+            .product::<isize>();
+
+        let (time, dist) = input
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .skip(1)
+                    .fold(String::new(), |acc, s| acc + &s)
+                    .parse::<isize>()
+                    .unwrap()
+            })
+            .collect_tuple()
             .unwrap();
 
-        let snd = (1..time).filter(|x| x * (time - x) > dist).count();
+        let snd = solve(time, dist);
 
         (fst.to_string(), snd.to_string())
     }
+}
+
+fn solve(time: isize, dist: isize) -> isize {
+    let (mut lo, mut hi) = roots(-1, time, -dist).unwrap();
+
+    while lo * (time - lo) <= dist {
+        lo += 1;
+    }
+
+    while hi * (time - hi) <= dist {
+        hi -= 1;
+    }
+
+    hi - lo + 1
+}
+
+fn roots(a: isize, b: isize, c: isize) -> Option<(isize, isize)> {
+    let delta = b * b - 4 * a * c;
+    if delta < 0 {
+        return None;
+    }
+
+    let x1 = (-b - delta.sqrt()) / (2 * a);
+    let x2 = (-b + delta.sqrt()) / (2 * a);
+
+    let (lo, hi) = (x1.min(x2), x1.max(x2));
+
+    Some((lo, hi))
 }
