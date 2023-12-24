e2d8af2ca73d171456e524c7e0ea9330b1e27291
mihneabuz <mihneabuzatu88@gmail.com>
2023-12-25 01:33:13 +0200

    added 2023 day23

---
+++ src/aoc2023/day23.rs
@@ -0,0 +1,136 @@
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+use smallvec::SmallVec;
+
+use lib::{aoc, challenge::Challenge, helpers::Bitset};
+
+pub struct Day23;
+
+type Graph = Vec<((usize, usize), Vec<(u32, usize)>)>;
+
+// TODO: optimize this
+impl Challenge for Day23 {
+    aoc!(year = 2023, day = 23);
+
+    fn solve(input: String) -> (String, String) {
+        let mut map = input
+            .lines()
+            .map(|line| line.as_bytes().to_vec())
+            .collect_vec();
+
+        let (n, m) = (map.len(), map[0].len());
+        map[0][1] = b'#';
+        map[n - 1][m - 2] = b'#';
+
+        let (start, end) = ((1, 1), (n - 2, m - 2));
+
+        let fst = {
+            let longest_path = |start: (usize, usize), end: (usize, usize)| {
+                let mut dists = vec![0; n * m];
+                let mut visiting = vec![(end, (end.0 + 1, end.1))];
+
+                while let Some((curr, prev)) = visiting.pop() {
+                    let dist = dists[curr.0 * m + curr.1];
+
+                    let neighs = [
+                        (curr.0 - 1, curr.1, b'^'),
+                        (curr.0 + 1, curr.1, b'v'),
+                        (curr.0, curr.1 - 1, b'<'),
+                        (curr.0, curr.1 + 1, b'>'),
+                    ];
+
+                    for (ni, nj, block) in
+                        neighs.into_iter().filter(|(ni, nj, _)| (*ni, *nj) != prev)
+                    {
+                        if ![b'#', block].contains(&map[ni][nj]) && dists[ni * m + nj] < dist + 1 {
+                            dists[ni * m + nj] = dist + 1;
+                            visiting.push(((ni, nj), curr));
+                        }
+                    }
+                }
+
+                dists[start.0 * m + start.1]
+            };
+
+            2 + longest_path(start, end)
+        };
+
+        let snd = {
+            let graph = convert(map, start);
+
+            let start_idx = graph.iter().position(|node| node.0 == start).unwrap();
+            let end_idx = graph.iter().position(|node| node.0 == end).unwrap();
+
+            fn dfs(idx: usize, end: usize, visited: Bitset<u64>, graph: &Graph) -> Option<u32> {
+                if idx == end {
+                    return Some(0);
+                }
+
+                let mut best = None;
+                for (cost, neigh) in graph[idx].1.iter() {
+                    if !visited.get(*neigh) {
+                        if let Some(rest) = dfs(*neigh, end, visited.set(idx), graph) {
+                            best = Some(best.unwrap_or(0).max(cost + rest));
+                        }
+                    }
+                }
+
+                best
+            }
+
+            dfs(start_idx, end_idx, Bitset::new(), &graph).unwrap() + 1
+        };
+
+        (fst.to_string(), snd.to_string())
+    }
+}
+
+fn convert(mut map: Vec<Vec<u8>>, start: (usize, usize)) -> Graph {
+    let mut graph = vec![(start, vec![])];
+    let mut queue = VecDeque::from_iter(Some((0, start)));
+
+    while let Some((idx, start)) = queue.pop_front() {
+        let mut dist = 1;
+        let mut pos = start;
+
+        loop {
+            map[pos.0][pos.1] = b'#';
+
+            let mut neighs: SmallVec<[_; 3]> = [
+                (pos.0 - 1, pos.1),
+                (pos.0 + 1, pos.1),
+                (pos.0, pos.1 - 1),
+                (pos.0, pos.1 + 1),
+            ]
+            .into_iter()
+            .filter(|&(ni, nj)| map[ni][nj] != b'#')
+            .collect();
+
+            if neighs.len() == 1 {
+                dist += 1;
+                pos = neighs.pop().unwrap();
+            } else {
+                let new_idx = match graph.iter().position(|node| node.0 == pos) {
+                    Some(idx) => idx,
+                    None => {
+                        let new_idx = graph.len();
+                        graph.push((pos, Vec::new()));
+                        new_idx
+                    }
+                };
+
+                graph[idx].1.push((dist, new_idx));
+                graph[new_idx].1.push((dist, idx));
+
+                for (ni, nj) in neighs {
+                    queue.push_back((new_idx, (ni, nj)));
+                }
+
+                break;
+            }
+        }
+    }
+
+    graph
+}
--- src/lib/helpers/bitset.rs
+++ src/lib/helpers/bitset.rs
@@ -1,8 +1,11 @@
-use std::ops::{BitAnd, BitOr, Shl};
+use std::{
+    mem::size_of,
+    ops::{BitAnd, BitOr, Shl},
+};
 
 use num::{Integer, PrimInt};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
 pub struct Bitset<T> {
     storage: T,
 }
@@ -22,6 +25,16 @@ where
     {
         Self {
             storage: self.storage | (T::one() << bit),
+        }
+    }
+
+    pub fn unset<B>(self, bit: B) -> Self
+    where
+        B: Ord,
+        T: Shl<B, Output = T> + BitOr<Output = T>,
+    {
+        Self {
+            storage: self.storage & !(T::one() << bit),
         }
     }
 
@@ -76,12 +89,51 @@ where
     }
 }
 
+#[derive(Clone, Debug, Hash, Eq, PartialEq)]
+pub struct BitArray {
+    inner: Vec<Bitset<usize>>,
+    size: usize,
+}
+
+impl BitArray {
+    pub fn new(size: usize) -> Self {
+        let buckets = size / size_of::<usize>() + 1;
+
+        Self {
+            inner: vec![Bitset::new(); buckets],
+            size,
+        }
+    }
+
+    pub fn get(&self, idx: usize) -> Option<bool> {
+        if idx >= self.size {
+            return None;
+        }
+
+        let bucket = idx / size_of::<usize>();
+        Some(self.inner[bucket].get(idx % size_of::<usize>()))
+    }
+
+    pub fn set(&mut self, idx: usize, value: bool) {
+        if idx >= self.size {
+            return;
+        }
+
+        let bucket = &mut self.inner[idx / size_of::<usize>()];
+        if value {
+            *bucket = bucket.set(idx % size_of::<usize>());
+        } else {
+            *bucket = bucket.unset(idx % size_of::<usize>());
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::Bitset;
+    use super::*;
 
     #[test]
-    fn create_bitmap() {
+    fn create_bitset() {
         let _: Bitset<u8> = Bitset::new();
         let _: Bitset<u16> = Bitset::new();
         let _: Bitset<u32> = Bitset::new();
@@ -99,6 +151,22 @@ mod tests {
         b = b.set(4);
 
         assert_eq!(b.storage, 27u128);
+    }
+
+    #[test]
+    fn unset_bits() {
+        let mut b: Bitset<u128> = Bitset::new();
+
+        b = b.set(1);
+        b = b.set(3);
+        b = b.set(4);
+
+        assert_eq!(b.storage, 26u128);
+
+        b = b.unset(3);
+        b = b.unset(4);
+
+        assert_eq!(b.storage, 2u128);
     }
 
     #[test]
@@ -155,5 +223,41 @@ mod tests {
         assert_eq!(b.get(7), true);
         assert_eq!(b.get(6), false);
         assert_eq!(b.count_bits(), 1);
+    }
+
+    #[test]
+    fn create_bitarray() {
+        let _ = BitArray::new(1);
+        let _ = BitArray::new(10);
+        let _ = BitArray::new(100);
+        let _ = BitArray::new(1000);
+    }
+
+    #[test]
+    fn get_set_bitarray() {
+        let mut array = BitArray::new(100);
+
+        array.set(0, true);
+        array.set(64, true);
+        array.set(80, true);
+        array.set(99, true);
+
+        assert_eq!(array.get(0), Some(true));
+        assert_eq!(array.get(1), Some(false));
+
+        assert_eq!(array.get(63), Some(false));
+        assert_eq!(array.get(64), Some(true));
+        assert_eq!(array.get(65), Some(false));
+
+        assert_eq!(array.get(80), Some(true));
+        assert_eq!(array.get(99), Some(true));
+
+        assert_eq!(array.get(100), None);
+
+        array.set(0, false);
+        array.set(80, false);
+
+        assert_eq!(array.get(0), Some(false));
+        assert_eq!(array.get(80), Some(false));
     }
 }
