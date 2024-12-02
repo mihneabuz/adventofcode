d8839085eb6758e85b4b55955c3c31fd4b97888d
mihneabuz <mihneabuzatu88@gmail.com>
2024-12-02 12:47:02 +0200

    fix result printing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod aoc2022;
 mod aoc2023;
+mod aoc2024;
 
 use std::time;
 
@@ -32,10 +33,14 @@ struct Args {
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    let mut challenges: Vec<ChallengeObject> = vec![aoc2022::challenges(), aoc2023::challenges()]
-        .into_iter()
-        .flatten()
-        .collect();
+    let mut challenges: Vec<ChallengeObject> = vec![
+        aoc2022::challenges(),
+        aoc2023::challenges(),
+        aoc2024::challenges(),
+    ]
+    .into_iter()
+    .flatten()
+    .collect();
 
     if let Some(year) = args.year {
         challenges.retain(|c| c.year == year);
@@ -85,46 +90,63 @@ fn main() -> std::io::Result<()> {
 }
 
 fn show_results(results: &[ChallengeResult]) {
-    use prettytable::{Cell, Row, Table};
+    use cli_table::{format::Justify, Cell, Color, Style, Table};
 
     if let Some(result) = results.iter().find(|r| r.example.is_some()) {
         let solution = result.example.as_ref().unwrap();
         println!("Example: {} {}", solution.0, solution.1);
     }
 
-    let mut table = Table::new();
-
-    let header = ["Year", "Day", "Part 1", "Part 2", "Time"]
-        .iter()
-        .map(|name| style(name).red().bold().to_string())
-        .collect::<Vec<_>>();
-
-    table.add_row(Row::new(
-        header.iter().map(|name| Cell::new(name)).collect(),
-    ));
-
     let longest = results
         .iter()
         .map(|r| r.duration)
         .max()
         .unwrap()
-        .as_micros()
+        .as_nanos()
         .ilog2();
 
+    let mut table = Vec::new();
     for result in results {
-        let duration = result.duration.as_micros().ilog2();
+        let duration = result.duration.as_nanos().ilog2();
         let rel_duration =
-            String::from("█").repeat((duration * 10 / longest.max(0)).max(1) as usize);
+            String::from("█").repeat((duration * 10 / longest.max(1)).max(1) as usize);
 
-        table.add_row(prettytable::row![
-            style(format!("{:?}", result.year)).blue(),
-            style(format!("{:?}", result.day)).green(),
-            result.solution.0,
-            result.solution.1,
-            style(format!("{:.2?}", result.duration)).yellow(),
-            style(rel_duration.to_string()).red(),
+        table.push(vec![
+            result
+                .year
+                .cell()
+                .foreground_color(Some(Color::Blue))
+                .justify(Justify::Right),
+            result
+                .day
+                .cell()
+                .foreground_color(Some(Color::Green))
+                .justify(Justify::Right),
+            result.solution.0.clone().cell(),
+            result.solution.1.clone().cell(),
+            format!("{:.2?}", result.duration)
+                .cell()
+                .foreground_color(Some(Color::Yellow))
+                .justify(Justify::Right),
+            rel_duration
+                .cell()
+                .foreground_color(Some(Color::Red))
+                .justify(Justify::Right),
         ]);
     }
 
-    table.printstd();
+    let header = ["Year", "Day", "Part 1", "Part 2", "Time", "Relative"]
+        .iter()
+        .map(|name| {
+            style(name)
+                .red()
+                .bold()
+                .to_string()
+                .cell()
+                .bold(true)
+                .justify(Justify::Center)
+        })
+        .collect::<Vec<_>>();
+
+    cli_table::print_stdout(table.table().title(header)).unwrap();
 }
