dfb35ca3172ebf3dcd56aec60334742612c45068
mihneabuz <mihneabuzatu88@gmail.com>
2024-12-11 17:23:28 +0200

    added 2024 day10

--- src/aoc2024/mod.rs
+++ src/aoc2024/mod.rs
@@ -13,7 +13,7 @@ pub fn challenges() -> Vec<ChallengeObject> {
         day7::Day7::into_obj(),
         day8::Day8::into_obj(),
         day9::Day9::into_obj(),
-        // day10::Day10::into_obj(),
+        day10::Day10::into_obj(),
         // day11::Day11::into_obj(),
         // day12::Day12::into_obj(),
         // day13::Day13::into_obj(),
